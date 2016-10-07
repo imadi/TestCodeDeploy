6c0090bb1bef7cafa40a8f862d7a9ba681b62ebf
Adithya Krishna <adithyakrishna.k@gmail.com>
2016-10-07 21:19:22 +0530

    Update start_application.sh

--- GrailsSample/scripts/start_application.sh
+++ GrailsSample/scripts/start_application.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mv /home/ubuntu/tomcat7/apache-tomcat-7.0.72/webapps/GrailsSample-0.1.war ROOT.war
+mv /home/ubuntu/tomcat7/apache-tomcat-7.0.72/webapps/GrailsSample-0.1.war /home/ubuntu/tomcat7/apache-tomcat-7.0.72/webapps/ROOT.war
 cd /home/ubuntu/tomcat7/apache-tomcat-7.0.72/bin
 ./startup.sh 
