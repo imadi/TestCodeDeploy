099a273c914f7ce6b340e63b4f8ad7529c29045f
Adithya Krishna <adithyakrishna.k@gmail.com>
2016-10-08 00:38:53 +0530

    Update rename_war.sh

--- GrailsSample/scripts/rename_war.sh
+++ GrailsSample/scripts/rename_war.sh
@@ -2,5 +2,6 @@
 
 cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/GrailsSample
 echo $PWD
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 chmod +x grailsw
 ./grailsw clean
