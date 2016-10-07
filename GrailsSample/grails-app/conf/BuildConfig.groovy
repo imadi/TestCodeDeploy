dede926fae44cb264e95ec2918503b122f89e015
Adithya <adithyakrishna.k@gmail.com>
2016-10-07 22:44:25 +0530

    removed work dir

--- GrailsSample/grails-app/conf/BuildConfig.groovy
+++ GrailsSample/grails-app/conf/BuildConfig.groovy
@@ -2,7 +2,6 @@ grails.servlet.version = "3.0" // Change depending on target container complianc
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-grails.project.work.dir = "target/work"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
