8e4de1f4f75d064d5e5c11f3a49d4aca3ad8fa2d
Adithya Krishna <adithyakrishna.k@gmail.com>
2016-10-07 23:55:04 +0530

    Update rename_war.sh

--- GrailsSample/scripts/rename_war.sh
+++ GrailsSample/scripts/rename_war.sh
@@ -5,5 +5,7 @@ echo $APPLICATION_NAME
 echo $DEPLOYMENT_GROUP_NAME
 echo $DEPLOYMENT_GROUP_ID
 echo $DEPLOYMENT_ID
+cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/GrailsSample
+echo $PWD
 chmod +x grailsw
 ./grailsw clean
