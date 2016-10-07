3ebce3307c0aaa7f64166f33a1bae9dcc17fc342
Adithya <adithyakrishna.k@gmail.com>
2016-10-07 18:14:24 +0530

    Logic Change

--- GrailsSample/scripts/stop_application.sh
+++ GrailsSample/scripts/stop_application.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-cd /home/ubuntu/tomcat7/apache-tomcat-7.0.72/bin
-/.shutdown.sh
+/etc/init.d/tomcat7 stop
+rm -rf /home/ubuntu/tomcat7/apache-tomcat-7.0.72/logs/*
+rm -rf /home/ubuntu/tomcat7/apache-tomcat-7.0.72/webapps/ROOT
+rm -rf /home/ubuntu/tomcat7/apache-tomcat-7.0.72/webapps/ROOT.war
